f5a297fe7d7dc6e2648934310008bcd72b75c959
matchaiscool <smedinamayo@gmail.com>
2026-09-01 01:18:12 +0000

    v1.0.2

--- src/account.py
+++ src/account.py
@@ -1,3 +1,13 @@
 from alpaca.trading.client import TradingClient
+from decimal import Decimal
+from dotenv import load_dotenv
+import os
 
-trading_client = TradingClient (,paper = True)
+load_dotenv()
+trading_client = TradingClient(os.getenv("API_KEY"),os.getenv("SECRET_KEY"),paper = True)  
+
+def get_account_snapshot(){
+    tc_info= trading_client.get_account()
+    
+}
+
